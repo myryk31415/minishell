0e8695b83e1fc23b7c31e215697100644273fe99
Myryk <myryk@posteo.de>
2024-02-14 18:11:59 +0100

    first draft of node structure

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 17:09:25 by padam             #+#    #+#             */
-/*   Updated: 2024/02/14 17:11:14 by padam            ###   ########.fr       */
+/*   Updated: 2024/02/14 18:08:41 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,21 @@
 
 # include "../libft/includes/libft.h"
 
+typedef struct s_node
+{
+	t_node_type		type;
+	char			*command;
+	struct s_node	*left;
+	struct s_node	*right;
+}	t_node;
+
+typedef enum e_node_type
+{
+	AND,
+	OR,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	CMD
+}	t_node_type;
 #endif
