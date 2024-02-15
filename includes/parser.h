4d643144f868187b7461b1db725f6a56a51ed6be
Myryk <myryk@posteo.de>
2024-02-15 21:39:20 +0100

    implemented prompt and history, prepared parser directory

--- includes/parser.h
+++ includes/parser.h
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/10 23:16:36 by padam             #+#    #+#             */
-/*   Updated: 2024/02/15 21:26:41 by padam            ###   ########.fr       */
+/*   Created: 2024/02/15 21:14:04 by padam             #+#    #+#             */
+/*   Updated: 2024/02/15 21:34:00 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef PARSER_H
+# define PARSER_H
 
-int	main(void)
-{
-	parser();
-	return (0);
-}
+# include "minishell.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+//colors
+# define RESET "\033[0m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+
+#endif
