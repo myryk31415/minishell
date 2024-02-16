39d7cf557c4eecde19ece27ff4e67a87ff074b6e
antonweizmann <aweizman@student.42heilbronn.de>
2024-02-16 12:49:45 +0100

    pipe tree sketch

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aweizman <aweizman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: antonweizmann <antonweizmann@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:43:09 by aweizman          #+#    #+#             */
-/*   Updated: 2024/02/15 16:49:01 by aweizman         ###   ########.fr       */
+/*   Updated: 2024/02/16 12:03:01 by antonweizma      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,48 @@ int	execution_start(t_node *token, t_redirect_in *input, t_redirect_out *output)
 int	execution(t_node *token, t_redirect_in *input, t_redirect_out *output)
 {
 	if (token->type == PIPE)
-		create_pipe(token->left, token->right);
+		create_tree(token->left, token->right);
 	if (token->type == CMD)
 		exec(token->command);
 }
 
-void	create_pipe(t_node left, t_node right)
+void	cmd(t_node *left, int *fd, int *pre_fd)
+{
+	dup2(fd[0], STDIN_FILENO);
+	dup2(pre_fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	close(pre_fd[0]);
+	close(pre_fd[1]);
+	exec(left->command);
+}
+
+void	input(t_redirect_in *input, int *pre_fd)
+{
+	int	file;
+
+	file = 0;
+
+}
+
+void	create_tree(int *pre_fd, t_node *token,
+		t_redirect_in *input, t_redirect_out *output)
 {
 	int	fd[2];
 	int	id;
 
-	+
+	if (pipe(fd) == -1)
+		perror("Pipe");
+	id = fork();
+	if (pid == -1)
+		perror("Fork");
+	if (pid == 0 && token->type_right == PIPE)
+		create_tree(fd, token->right, input, output);
+	else if (pid != 0 && token->type_left == REDIR_IN)
+		input(, fd);
+	else if (pid != 0 && token->type_right == REDIR_OUT)
+		output(args, fd);
+	else
+		cmd(token->left, fd, pre_fd);
+
 }
