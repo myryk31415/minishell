118bb838e67c65b1d36aa831bfa992650ce888c5
Myryk <myryk@posteo.de>
2024-02-25 20:27:51 +0100

    still implementing node structure

--- src/parser/node_struct.c
+++ src/parser/node_struct.c
@@ -6,11 +6,35 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 19:38:13 by padam             #+#    #+#             */
-/*   Updated: 2024/02/23 01:11:07 by padam            ###   ########.fr       */
+/*   Updated: 2024/02/24 12:26:51 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+t_token	*get_right_paren(t_token *tokens)
+{
+	int	paren_level;
+
+	paren_level = 1;
+	tokens = tokens->next
+	while (paren_level && tokens)
+	{
+		if ()
+	}
+}
+
+t_token_type	get_next_node(t_token *tokens, t_node *n_top)
+{
+	// t_node 		*node_left;
+	// t_node_type	node_type_left;
+	// t_node 		*node_right;
+	// t_node		node_type_right;
+
+	if (token->type == T_WORD)
+	if (tokens->type == T_PARENTHESIS_OPEN)
+		n_top->type_left == get_next_node(tokens->next, n_top->left);
+}
 
 t_node_group	*tokens_to_tree(t_token *tokens)
 {
--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,14 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:48:38 by padam             #+#    #+#             */
-/*   Updated: 2024/02/20 17:50:43 by padam            ###   ########.fr       */
+/*   Updated: 2024/02/24 12:03:26 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "parser.h"
 
 int	is_quote(char c)
 {
@@ -18,4 +20,31 @@ int	is_quote(char c)
 int	is_separator(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+/*
+ * @brief removes one token from linked list
+*/
+void	delete_token(t_token **token)
+{
+	t_token	*tmp;
+
+	free((*token)->value);
+	tmp = *token;
+	*token = (*token)->next;
+	free(tmp);
+}
+
+/*
+ * @brief splits a token linked list in two parts
+ * @param tokens the token after which the list is split
+ * @return first token of new list
+*/
+t_token	*split_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	tmp = tokens->next;
+	tokens->next = NULL;
+	return (tmp);
 }
