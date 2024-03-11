8629d1530f400a1b0d2fb74c313b72ea1a01eb5c
Myryk <myryk@posteo.de>
2024-03-12 00:35:11 +0100

    bugfixes, doesnt close fd1 anymore

--- src/parser/binary_tree/node_struct.c
+++ src/parser/binary_tree/node_struct.c
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 19:38:13 by padam             #+#    #+#             */
-/*   Updated: 2024/03/11 21:01:22 by padam            ###   ########.fr       */
+/*   Updated: 2024/03/12 00:30:23 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ t_node_type	check_brackets(t_token *token_first, void **head,
 {
 	t_token			*token_last;
 	t_cmd			*redirects;
-	t_redir	*new_node;
+	t_redir			*new_node;
 
 	if (!token_first)
 		return (ERROR);
@@ -68,7 +68,9 @@ t_node_type	check_brackets(t_token *token_first, void **head,
 			cmd_free(redirects);
 			return (ERROR);
 		}
+		new_node->redirects = redirects;
 		new_node->type = split_by_operator(token_last, &new_node->next, NULL);
+		*head = new_node;
 		return (REDIR);
 	}
 	return (get_cmd(token_first, head, redirects));
