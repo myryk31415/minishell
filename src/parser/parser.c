4ee615aff1649ba341ca4954da5af0a4b90748d5
Myryk <myryk@posteo.de>
2024-03-29 23:22:00 +0100

    added first tester

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: padam <padam@student.42heilbronn.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 21:15:56 by padam             #+#    #+#             */
-/*   Updated: 2024/03/28 17:03:30 by padam            ###   ########.fr       */
+/*   Updated: 2024/03/29 22:51:34 by padam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,12 +45,12 @@ char	*new_prompt(char **env)
 	free(prompt);
 	prompt = ft_strjoin(prompt_tmp, "$ ");
 	free(prompt_tmp);
-	// command = readline(prompt);
-	ft_putstr_fd(prompt, 0);
-	command = get_next_line(0);
+	command = readline(prompt);
+	// ft_putstr_fd(prompt, 0);
+	// command = get_next_line(0);
 	free(prompt);
-	// if (command && *command && *command != '\n')
-	// 	add_history(command);
+	if (command && *command && *command != '\n')
+		add_history(command);
 	return (command);
 }
 
