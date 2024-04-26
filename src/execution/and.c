ff5c21ef6dd92fd4b311af535c5251027f6b09bb
antonweizmann <aweizman@student.42heilbronn.de>
2024-04-26 13:33:14 +0200

    it WOOONRKsgit add .!

--- src/execution/and.c
+++ src/execution/and.c
@@ -6,7 +6,7 @@
 /*   By: antonweizmann <antonweizmann@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 10:39:18 by aweizman          #+#    #+#             */
-/*   Updated: 2024/04/26 13:27:34 by antonweizma      ###   ########.fr       */
+/*   Updated: 2024/04/26 13:32:38 by antonweizma      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	and_left(t_node *token, int status, int **pipes, t_exec *exec)
 void	and_execute(t_node *token, int status, int **pipes, t_exec *exec)
 {
 	and_left(token, status, pipes, exec);
-	if (!exec->exit_status && pipes && token->type_right == CMD)
+	if (!exec->exit_status && token->type_right == CMD)
 		command((t_cmd *)token->right, pipes, 2, exec);
 	else if (!exec->exit_status && token->type_right == PIPE)
 		exec->exit_status = create_tree(0, (t_node *)token->right, *exec, pipes);
@@ -45,7 +45,8 @@ void	and_execute(t_node *token, int status, int **pipes, t_exec *exec)
 		exec->exit_status = redirect((t_redir *)token->right, pipes, 0, *exec);
 	else if (!exec->exit_status && token->type_right == ERROR)
 		exec->exit_status = 2;
-	close(pipes[0][1]);
+	if (pipes && pipes[0])
+		close(pipes[0][1]);
 }
 
 void	or_left(t_node *token, int status, int **pipes, t_exec *exec)
